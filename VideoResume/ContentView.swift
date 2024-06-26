8d53e84010cbadef7b945abfdd78eca9abec9710
Nathan Merz <NMerz@users.noreply.github.com>
2024-06-26 12:29:56 -0600

    Initial Commit

---
+++ VideoResume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoResume
+//
+//  Created by Nathan Merz on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoResume/VideoResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoResumeApp.swift
+//  VideoResume
+//
+//  Created by Nathan Merz on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
