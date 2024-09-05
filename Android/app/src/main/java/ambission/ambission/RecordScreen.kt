e2d2c2ea14fe324fed64a26f28946f9603ff1380
Nathan Merz <NMerz@users.noreply.github.com>
2024-09-05 11:18:59 -0600

    Add Android teleprompting

--- Android/app/src/main/java/ambission/ambission/RecordScreen.kt
+++ Android/app/src/main/java/ambission/ambission/RecordScreen.kt
@@ -1,13 +1,10 @@
 package ambission.ambission
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.ContentValues
-import android.content.Context
 import android.content.pm.PackageManager
-import android.database.Cursor
-import android.net.Uri
 import android.provider.MediaStore
+import android.util.DisplayMetrics
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -21,42 +18,41 @@ import androidx.camera.view.CameraController.VIDEO_CAPTURE
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.camera.view.video.AudioConfig
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CameraAlt
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
-import androidx.graphics.shapes.RoundedPolygon
-import androidx.graphics.shapes.toPath
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerView
+import androidx.media3.extractor.text.webvtt.WebvttCssStyle.FontSizeUnit
 import kotlinx.serialization.Serializable
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Objects
+import kotlin.math.max
+import kotlin.math.min
 
 
 class RecordViewModel: DatabaseAccess() {
@@ -71,19 +67,6 @@ class RecordViewModel: DatabaseAccess() {
 
 @Serializable
 class RecordScreenArgs(val segmentId: String, val videoUid: String) {
-}
-
-
-fun Context.createImageFile(): File {
-    // Create an image file name
-    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-    val imageFileName = "MOV_" + timeStamp + "_"
-    val movie = File.createTempFile(
-        imageFileName, /* prefix */
-        ".3gp", /* suffix */
-        this.filesDir      /* directory */
-    )
-    return movie
 }
 
 @Composable
@@ -114,24 +97,35 @@ fun RecordScreen(args: RecordScreenArgs, navFunction: (Any) -> Unit, returnFunct
         }
     )
 
-    Box(
-        modifier = Modifier
-            .drawWithCache {
-                val roundedPolygon = RoundedPolygon(
-                    numVertices = 6,
-                    radius = size.minDimension / 2,
-                    centerX = size.width / 2,
-                    centerY = size.height / 2
-                )
-                val roundedPolygonPath = roundedPolygon
-                    .toPath()
-                    .asComposePath()
-                onDrawBehind {
-                    drawPath(roundedPolygonPath, color = Color.Blue)
-                }
-            }
-            .fillMaxSize()
+    val isRecording = remember{ mutableStateOf(false) }
+    var scrollSpeed by remember { mutableIntStateOf(0) }
+
+    var textBoxSize by remember { mutableIntStateOf(0) }
+    var textSize by remember { mutableIntStateOf(0) }
+
+
+//    with(LocalDensity.current) { LocalConfiguration.current.screenHeightDp.dp.toPx() }
+    val offset: Float by animateFloatAsState(
+        targetValue = if (isRecording.value) textBoxSize.toFloat() * -1 else 0.0F,
+        // Configure the animation duration and easing.
+        animationSpec = tween(durationMillis = if (isRecording.value) max(scrollSpeed, 1000) else 1, easing = LinearEasing), label = ""
     )
+    vm.getSegmentTexts(args.videoUid)[args.segmentId]?.let { Text(text = it, modifier = Modifier.offset { IntOffset(0,
+        offset.toInt()
+    )}.onSizeChanged{
+        textBoxSize = it.height
+    }, fontSize = max(textSize, 12).sp, lineHeight = max(textSize, 12).sp) }
+    Slider(
+        value = scrollSpeed.toFloat() / 30000F,
+        onValueChange = { scrollSpeed = (it * 30000).toInt() },
+        modifier = Modifier.rotate(90F).offset((LocalConfiguration.current.screenHeightDp * 0.5).dp, (LocalConfiguration.current.screenWidthDp * -0.4).dp)
+    )
+
+    Slider(
+        value = textSize.toFloat() / 192.0F,
+        onValueChange = { textSize = (it * 192.0).toInt() }
+    )
+
     val localContext = LocalContext.current
 
     val videoFileName = "video_" + System.currentTimeMillis()
@@ -165,22 +159,25 @@ fun RecordScreen(args: RecordScreenArgs, navFunction: (Any) -> Unit, returnFunct
         ) {
             permissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
 
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return@IconButton
+            return@IconButton //TODO: resume instead
+        }
+
+        val permissionCheckResult =
+            ContextCompat.checkSelfPermission(localContext, Manifest.permission.CAMERA)
+        if (permissionCheckResult != PackageManager.PERMISSION_GRANTED) {
+            // Request a permission
+            permissionLauncher.launch(Manifest.permission.CAMERA)
+            return@IconButton //TODO: resume instead
         }
 
         cameraController.setEnabledUseCases(VIDEO_CAPTURE)
         if (cameraController.isRecording() && recording.value != null) {
             // Stop the current recording session.
+            isRecording.value = false
             Log.d("RecordScreen", "Stopping recording")
             recording.value?.stop()
         } else {
+            isRecording.value = true
             Log.d("RecordScreen", "Starting recording")
             recording.value = cameraController.startRecording(fileOptions, AudioConfig.create(true), ContextCompat.getMainExecutor(localContext)) { videoRecordEvent ->
                 if (videoRecordEvent is VideoRecordEvent.Finalize) {
@@ -207,83 +204,5 @@ fun RecordScreen(args: RecordScreenArgs, navFunction: (Any) -> Unit, returnFunct
             imageVector = Icons.Default.CameraAlt,
             contentDescription = ""
         )
-    }
-}
-
-@Composable
-fun CameraPreview() {
-
-
-}
-
-
-@Composable
-fun RecordScreenOverlayed(args: RecordScreenArgs, navFunction: (Any) -> Unit, returnFunction: () -> Boolean, modifier: Modifier, vm: RecordViewModel) {
-    val localContext = LocalContext.current
-
-    val file = localContext.createImageFile()
-    val uri = FileProvider.getUriForFile(
-        Objects.requireNonNull(localContext),
-        localContext.packageName + ".provider", file
-    )
-
-    var capturedImageUri by remember {
-        mutableStateOf<Uri>(Uri.EMPTY)
-    }
-
-    val cameraLauncher =
-        rememberLauncherForActivityResult(ActivityResultContracts.CaptureVideo()) {wasSaved ->
-            if (wasSaved) {
-//                capturedImageUri = uri
-                uri.lastPathSegment?.let { vm.setSegmentUrl(args.videoUid, args.segmentId, it) }
-                returnFunction()
-            }
-        }
-
-    val permissionLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) {
-        if (it) {
-            Toast.makeText(localContext, "Permission Granted", Toast.LENGTH_SHORT).show()
-            cameraLauncher.launch(uri)
-        } else {
-            Toast.makeText(localContext, "Permission Denied", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    Column(
-        Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            val permissionCheckResult =
-                ContextCompat.checkSelfPermission(localContext, Manifest.permission.CAMERA)
-            if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
-                cameraLauncher.launch(uri)
-            } else {
-                // Request a permission
-                permissionLauncher.launch(Manifest.permission.CAMERA)
-            }
-        }) {
-            Text(text = "Capture Image From Camera")
-        }
-    }
-
-    if (capturedImageUri.path?.isNotEmpty() == true) {
-        AndroidView(
-            modifier = Modifier.fillMaxSize(),
-            factory = { context ->
-                val player = ExoPlayer.Builder(localContext).build()
-                MediaItem.fromUri(capturedImageUri)
-                    .let { player.setMediaItem(it) }
-                player.prepare()
-                player.play()
-                val playerView = PlayerView(context)
-                playerView.player = player
-                playerView
-            }
-        )
-
     }
 }
